adc8041741911f8625a6752c3b760303a6909108
Ignacio Solar <ignacio.solar@usach.cl>
2024-10-17 19:48:37 -0300

    Update cut.c

--- cut.c
+++ cut.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// funcion que maneja las columnas (-c), columnas = columnas que seran extraidas del archivo de entrada para ser 
-// retornadas en output. (definida mas abajo)
+// funcion que maneja las columnas (-c), columnas = columnas que seran extraidas
+// del archivo de entrada para ser retornadas en output. (definida mas abajo)
 int parse_columns(char *optarg, int **columns, int *num_columns);
 
-// funcion que extrae columnas del archivo de entrada para retornarlas por el de salida. (definida mas abajo)
+// funcion que extrae columnas del archivo de entrada para retornarlas por el de
+// salida. (definida mas abajo)
 void extract_columns(FILE *in, FILE *out, char sep, int *columns,
                      int num_columns, int all_columns);
 
 // Entrada: recibe 2 strings.
 // Salida: retorna 1 si los strings son iguales, caso contrario, retorna 0.
-// Descripción: compara ambos strings entregados, si son iguales retorna 1, sino 0.
+// Descripción: compara ambos strings entregados, si son iguales retorna 1, sino
+// 0.
 int str_compare(char *s1, char *s2) {
     while (*s1 && *s2 && *s1 == *s2) {
         s1++;
@@ -38,7 +40,7 @@ int main(int argc, char *argv[]) {
                 output_file = optarg;
                 break;
             case 'd':
-                if (optarg && optarg[0] != '\0') { 
+                if (optarg && optarg[0] != '\0') {
                     if (optarg[0] == 'b' && optarg[1] == '\0') {
                         sep = ' ';
                     } else if (optarg[0] == 't' && optarg[1] == '\0') {
@@ -87,10 +89,13 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-// Entradas: recibe un string (columnas a extraer del archivo), un arreglo que almacena columnas (**columns apunta a un arreglo)
-//           y un puntero (*num_columns) que almacena la cantidad de columnas que cumplen con lo solicitado.
-// Salidas:  retorna 0 si la ejecucion fue correcta, por el contrario, retorna 1 si existen errores.
-// Descripcion: convierte un string en una lista (arreglo) de columnas que deben ser extraidas del archivo, si se genera
+// Entradas: recibe un string (columnas a extraer del archivo), un arreglo que
+// almacena columnas (**columns apunta a un arreglo)
+//           y un puntero (*num_columns) que almacena la cantidad de columnas
+//           que cumplen con lo solicitado.
+// Salidas:  retorna 0 si la ejecucion fue correcta, por el contrario, retorna 1
+// si existen errores. Descripcion: convierte un string en una lista (arreglo)
+// de columnas que deben ser extraidas del archivo, si se genera
 //              un error retorna un mensaje retroalimentando por consola.
 int parse_columns(char *optarg, int **columns, int *num_columns) {
     if (optarg == NULL) {
@@ -155,26 +160,59 @@ int parse_columns(char *optarg, int **columns, int *num_columns) {
     return 0;
 }
 
-// Entradas: recibe el archivo de entrada (input_file), archivo de salida (output_file), el separador de las columnas que se
-//           desean extraer, una lista (arreglo) con los numeros y cantidad de columnas a extraer, y finalmente, un flag que 
-//           indica si todas las columnas deben ser extraidas.
-// Salidas:  retorna mediante el archivo de salida, las columnas que fueron extraidas dependiendo del separador señalado.
-// Descripción: lee el archivo de entrada linea por linea, separando cada una de estas en columnas dependiendo del separador
-//              solicitado, para luego retornarlas mediante el archivo de salida. Si no se especifican columnas, se retornaran 
-//              todas las columnas del archivo de entrada al de salida. (all_columns)
+// Entradas: recibe el archivo de entrada (input_file), archivo de salida
+// (output_file), el separador de las columnas que se
+//           desean extraer, una lista (arreglo) con los numeros y cantidad de
+//           columnas a extraer, y finalmente, un flag que indica si todas las
+//           columnas deben ser extraidas.
+// Salidas:  retorna mediante el archivo de salida, las columnas que fueron
+// extraidas dependiendo del separador señalado. Descripción: lee el archivo de
+// entrada linea por linea, separando cada una de estas en columnas dependiendo
+// del separador solicitado, para luego retornarlas mediante el archivo de
+//              salida. Si no se especifican columnas, se retornaran todas las
+//              columnas del archivo de entrada al de salida. (all_columns)
 void extract_columns(FILE *in, FILE *out, char sep, int *columns,
                      int num_columns, int all_columns) {
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+    int line_number = 0;
 
     while ((read = getline(&line, &len, in)) != -1) {
+        line_number++;
         int col_count = 1;
-        int print_count = 0;
-        char *start = line;
         char *ptr = line;
 
-        while (*ptr) { // divide las lineas en columnas
+        // cantidad de columnas en la linea, para verificar que no se pase de
+        // largo el separador pedido
+        for (; *ptr; ptr++) {
+            if (*ptr == sep) {
+                col_count++;
+            }
+        }
+
+        // revisar que la columna este en el rango de la linea
+        if (!all_columns) {
+            for (int i = 0; i < num_columns; i++) {
+                if (columns[i] > col_count) {
+                    fprintf(stderr,
+                            "Error: La columna %d excede el numero de columnas "
+                            "en el archivo "
+                            "(%d) en la linea %d\n",
+                            columns[i], col_count, line_number);
+                    free(line);
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+
+        // procesamiento normal
+        int print_count = 0;
+        char *start = line;
+        ptr = line;
+        col_count = 1;
+
+        while (*ptr) {
             if (*ptr == sep || *ptr == '\n') {
                 char temp = *ptr;
                 *ptr = '\0';
